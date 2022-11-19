0a38973124cdf64547e259724ffd47869c7283fc
Christian Martin <fxgx99@gmail.com>
2022-11-20 00:42:25 +0100

    Initialized the ChrisMartain Enterprise TechBlog

---
+++ src/test/java/com/chrismartain/springbootblogapplication/SpringBootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrismartain.springbootblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
