274c2482f25504b16075508c8ae887a49f10647e
christian <fxgx99@gmail.com>
2022-11-20 01:31:26 +0100

    - First Successful Build - Lombok require annotation enabled - Find way to enable Lombok permanently.

--- src/main/java/com/chrismartain/springbootblogapplication/SpringBootBlogApplication.java
+++ src/main/java/com/chrismartain/springbootblogapplication/SpringBootBlogApplication.java
@@ -10,4 +10,8 @@ public class SpringBootBlogApplication {
 		SpringApplication.run(SpringBootBlogApplication.class, args);
 	}
 
+	// Compiled Successfully several times but Lombok error annotation keeps popping up.
+	// Lombok Message: Lombok require annotation enabled do you want to enable?
+	// Have to find a way to permanently enable annotation for lombok
+
 }
